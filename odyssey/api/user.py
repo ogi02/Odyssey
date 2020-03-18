2b934634e4de1a3350b5e397ff430994e4b50b54
Gabriela Yontcheva <gabrielay2002@gmail.com>
2020-03-18 19:56:44 +0200

    [Update] users.py added index for creators and functions to handle it
    Signed-off-by: Gabriela Yontcheva <gabrielay2002@gmail.com>

--- odyssey/api/user.py
+++ odyssey/api/user.py
@@ -2,27 +2,45 @@ from flask import jsonify
 from pymongo import MongoClient
 from passlib.hash import sha256_crypt
 from bson import ObjectId
+from pymongo import ReturnDocument
 
 client = MongoClient("mongodb+srv://KelpieG:admin11@clusterodyssey-olnzj.mongodb.net/test?retryWrites=true&w=majority")
 db = client.user
 
 class User:
-	def __init__(self, _id, username, password, name, email):
+	def __init__(self, _id, username, password, name, email, is_creator):
 		self._id = _id
 		self.username = username
 		self.password = password
 		self.name = name
 		self.email = email
+		self.is_creator = is_creator
 
 	def create(self):
 		user = {
 			'username': self.username,
 			'password': self.password,
 			'name': self.name,
-			'email': self.email
+			'email': self.email,
+			'is_creator': False
 		}
 		result = db.users.insert_one(user)
 		return self
+
+	def create_creator(self):
+		user = {
+			'username': self.username,
+			'password': self.password,
+			'name': self.name,
+			'email': self.email,
+			'is_creator': True
+		}
+		result = db.users.insert_one(user)
+		return self
+
+	def update_to_creator(username):
+
+		found = db.users.find_one_and_update({'username': username}, {"$set": {'is_creator': True}}, return_document=ReturnDocument.AFTER)
 
 	def find_by_username(username):
 		found = db.users.find_one({'username': username})
@@ -46,3 +64,4 @@ class User:
 
 	def verify_password(self, password):
 		return sha256_crypt.verify(password, self.password)
+
