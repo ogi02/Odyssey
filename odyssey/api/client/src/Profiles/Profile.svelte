dffe4ff72fa575102b0c477432700ee767cc25ce
GabrielaY <gabrielay2002@gmail.com>
2020-03-24 21:21:35 +0200

    [New] Started working on profile pages. Updated main.py and found how to use GET method

---
+++ odyssey/api/client/src/Profiles/Profile.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { fetchGet } from '../fetch.js';
+	import { onMount } from 'svelte';
+	onMount(()=>{
+		get_user();
+	});
+	let user = {};
+	async function get_user(){
+		const response = await fetchGet("/profile");
+		user = response.user;
+		console.log(user);
+	}
+	
+</script>
+<p>{user.username}</p>
