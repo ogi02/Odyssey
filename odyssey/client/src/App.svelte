1c68ae32d539cc3d6b2aeb86ba805e6a6f873698
GabrielaY <gabrielay2002@gmail.com>
2020-03-27 18:05:18 +0200

    [New] Testing routing methods with svelte routing library

--- odyssey/client/src/App.svelte
+++ odyssey/client/src/App.svelte
@@ -1,13 +1,17 @@
 <script>
-	
+
+	import { Router, Route } from "svelte-routing";
+	import NavLink from "./components/NavLink.svelte";
 	import { onMount } from 'svelte';
 	import Authentication from './Authentication/Authentication.svelte';
 	import BecomeCreator from './Authentication/BecomeCreator.svelte'
 	import Profile from './Profiles/Profile.svelte'
+	export let url = "";
 	
 	let loggedIn;
 	
 	onMount(async () => {
+
 		const res = await fetch(
 			'http://localhost:3000/checkLogin');
 		const response = await res.json();
@@ -15,14 +19,27 @@
 			loggedIn = true;
 		} else {
 			loggedIn = false;
-		}	
+		}		
 		console.log(loggedIn);
 	});
+	
 
 </script>
-
 {#if loggedIn == false}
+	<p> Welcome to Odyssey! Log in or register to view content.</p>
 	<Authentication />
 {:else}
-	<Profile />
+	<Router url="{url}">
+	  <nav>
+	    <NavLink to="/">Profile</NavLink>
+	    <NavLink to="about">About</NavLink>
+	    <NavLink to="become_a_creator">Become a creator!</NavLink>
+	  </nav>
+	  <div>
+	    <Route path="about" component="{Profile}" />
+	    <Route path="become_a_creator" component="{BecomeCreator}" />
+	    <Route path="/" component="{Profile}" />
+	  </div>
+	</Router>
 {/if}
+
---
+++ odyssey/client/src/components/NavLink.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Link } from "svelte-routing";
+
+  export let to = "";
+
+  function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+    const isActive = href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
+
+    // The object returned here is spread on the anchor element's attributes
+    if (isActive) {
+      return { class: "active" };
+    }
+    return {};
+  }
+</script>
+<div style="position: relative; top: 500px;">
+  <Link to="{to}" getProps="{getProps}">
+    <slot />
+  </Link>
+</div>
