b96ccbcb7792e061e4cf45bf8bdd976f191000a1
GabrielaY <gabrielay2002@gmail.com>
2020-03-31 14:59:38 +0300

    [Update] Updated UI for profile page and navbar

--- odyssey/client/src/Home.svelte
+++ odyssey/client/src/Home.svelte
@@ -1,8 +1,28 @@
-<p>Welcome to Odyssey</p>
-
+<div class="info">
+	<p>What is Odyssey?</p>
+</div>
 <style>
-	p {
-		position: relative;
-		top: 50px;
+	.info{
+		position: absolute;
+		width: 100%;
+		background-color: purple;
+		left: 0; 
+		top: 5%;
+  		right: 0;
+  		margin: auto; 
+  		min-width: 50%;
+  		min-height: 50%;
+  		z-index: -1;
+
 	}
+	.info p{
+		position: relative;
+		color: white;
+		text-align: center;
+		justify-content: center;
+		top: 3em;
+		font-size: 1.7em;
+
+	}
+
 </style>
