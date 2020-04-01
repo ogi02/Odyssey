0b9d760ed7903895cfafe9848b12f6b38e2bdf00
GabrielaY <gabrielay2002@gmail.com>
2020-04-01 12:52:23 +0300

    [Update] Added a banner to homepage + an introduction to all creator types (big UI update)

--- odyssey/client/src/Home.svelte
+++ odyssey/client/src/Home.svelte
@@ -1,28 +1,179 @@
-<div class="info">
-	<p>What is Odyssey?</p>
+
+<div>
+	<div class="full-width-first">
+		<div class="wrap">
+			<div>
+				<p style="text-align: left; font-size: 3.1em">Be creative!</p>
+		  		<p style="max-width: 16em; font-size: 1.1em;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+		  		<button class="button-join">Join Odyssey today!</button>
+			</div>
+			<div class="image-container">
+				<div>
+					 	<img class="image-in-container" src="https://i.pinimg.com/originals/f6/5a/53/f65a53be9515951bf9e28504efc5d6b0.jpg"
+						style="width: 9em; height: 12em">
+						<img class="image-in-container" src="https://cdn.shopify.com/s/files/1/1734/8431/products/Flying_1024x1024.jpg?v=1491498378"
+						style="width: 9em; height: 12em">
+						<img class="image-in-container" src="https://i.pinimg.com/originals/28/72/54/2872549acfb2e0ee8846edc3f973fd2a.jpg"
+						style="width: 9em; height: 12em">
+				</div>
+				<div>
+						<img  class="image-in-container" src="https://images.discerningassets.com/image/upload/c_fit,h_1000,w_1000/c_fit,fl_relative,h_1.0,o_100,w_1.0/v1559837064/Giraffe_Gerald_qh7k1j.jpg"
+						style="width: 9em; height: 12em">
+						<img  class="image-in-container" src="https://i.pinimg.com/originals/07/4c/e9/074ce9b21ed92eaaa6a759dd3863b069.jpg"
+						style="width: 9em; height: 12em">
+						<img  class="image-in-container" src="https://cdn.shopify.com/s/files/1/1734/8431/products/White_Rooster_1024x1024.jpg?v=1581350689"
+						style="width: 9em; height: 12em;">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="full-width-first" style="background-color: white; color: #080d52;">
+		<div class="wrap-text">
+			<p style="font-size: 2em;">Join today as a...</p>
+		</div>
+		
+	</div>
+	<div class="full-width-first" style="background-color: white; color: #080d52;">
+
+	  <div class="wrap">
+	  	<div>
+	  		<i class="fa fa-video-camera" aria-hidden="true"></i>
+	  		<span>Video Creator</span>
+	  		<p style="max-width: 8em; text-align: justify-all;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  	<div>
+	  		<i class="fa fa-paint-brush" aria-hidden="true"></i>
+	  		<span>Visual Artist</span>
+	  		<p style="max-width: 8em; text-align:justify-all; ;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  	<div>
+	  		<i class="fa fa-podcast" aria-hidden="true"></i>
+	  		<span>Podcaster</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  	<div>
+	  		<i class="fa fa-music" aria-hidden="true"></i>
+	  		<span>Musician</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  	<div>
+	  		<i class="fa fa-book" aria-hidden="true"></i>
+	  		<span>Writer/Journalist</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  </div>
+	  <div class="wrap" style="padding-bottom: 4em">
+	  	<div>
+	  		<i class="fa fa-gamepad" aria-hidden="true"></i>
+	  		<span>Gaming Creator</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  	<div>
+	  		<i class="fa fa-cutlery" aria-hidden="true"></i>
+	  		<span>Culinary artist</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  		  	<div>
+	  		<i class="fa fa-shopping-basket" aria-hidden="true"></i>
+	  		<span>Local Buissines</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  	<div>
+	  		<i class="fa fa-handshake-o" aria-hidden="true"></i>
+	  		<span>Non-profit</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  	<div>
+	  		<i class="fa fa-plus-square" aria-hidden="true"></i>
+	  		<span>Something else</span>
+	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  	</div>
+	  </div>
+	</div>
+
+	<div class="full-width-first" style="background-color: #080d52; color: white">
+	  <div class="wrap">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero</div>
+	</div>
 </div>
 <style>
-	.info{
-		position: absolute;
-		width: 100%;
-		background-color: purple;
-		left: 0; 
-		top: 5%;
-  		right: 0;
-  		margin: auto; 
-  		min-width: 50%;
-  		min-height: 50%;
-  		z-index: -1;
+.button-join {
+	border-radius: 26px;
+	background-color: #e0f4ff;
+	border-width: 0;
+	color: #080d52;
+	font-weight: 450;
+	font-size: 1.1em;
+	margin-top: 1em;
+	padding: 0.7em;
+	transition: 0.3s;
+}
+.button-join:hover{
+	background-color: #f9c3fa;
+	border-color: white;
+	border-width: 1px;
+}
+.image-in-container{
+	padding: 0.3em;
+}
+.image-containter {
+	display: flex;
+	flex-wrap: wrap;
+	 
+}
+.wrap {
+	justify-content: space-between;
+	align-items: flex-start;
+	display: flex;
+	flex-wrap: nowrap;	
+	flex-direction: row;
+	width: 80%;
+	max-width: 50em;
+	margin: 0 auto;
+	text-align: left;
+	padding: 1em;
 
-	}
-	.info p{
-		position: relative;
-		color: white;
-		text-align: center;
-		justify-content: center;
-		top: 3em;
-		font-size: 1.7em;
+	
+	
+}
+.wrap-text{
+	justify-content: center;
+	align-items: center;
+	display: flex;
+	flex-wrap: nowrap;	
+	flex-direction: row;
+	width: 80%;
+	max-width: 50em;
+	margin: 0 auto;
+	text-align: left;
+	
 
-	}
+	
+	
+}
+html, body {
+  overflow: hidden;
+}
 
+.full-width {
+	width: 100vw;
+	display: block;
+	position: relative;
+	left: 50%;
+	right: 50%;
+	margin: 0;
+	margin-left: -50vw;
+	background-color: white;
+	margin-right: -50vw;
+}
+.full-width-first {
+	top: 2.5em;
+	width: 100vw;
+	position: relative;
+	left: 50%;
+	color: white;
+	right: 50%;
+	margin-left: -50vw;
+	background-color: #080d52;
+	margin-right: -50vw;
+}
 </style>
