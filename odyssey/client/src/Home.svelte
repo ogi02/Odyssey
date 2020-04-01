0904dfe584f28fedb7cf88efe5b585456433aebe
GabrielaY <gabrielay2002@gmail.com>
2020-04-01 15:45:38 +0300

    [Update] Fixed scroll bar + added a custom scrollbar + converted inline CSS to class

--- odyssey/client/src/Home.svelte
+++ odyssey/client/src/Home.svelte
@@ -1,101 +1,124 @@
 
+<head>
+	<style>
+	/* width */
+	::-webkit-scrollbar {
+	width: 10px;
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+	background: #f1f1f1; 
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+	background: grey; 
+	}
+
+	</style>
+</head>
+<body>
 <div>
-	<div class="full-width-first">
+	<div class="full-width-first" class="dark-field">
 		<div class="wrap">
 			<div>
-				<p style="text-align: left; font-size: 3.1em">Be creative!</p>
-		  		<p style="max-width: 16em; font-size: 1.1em;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+				<p class="motto">Be creative!</p>
+		  		<p class="slogan">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 		  		<button class="button-join">Join Odyssey today!</button>
 			</div>
 			<div class="image-container">
 				<div>
-					 	<img class="image-in-container" src="https://i.pinimg.com/originals/f6/5a/53/f65a53be9515951bf9e28504efc5d6b0.jpg"
-						style="width: 9em; height: 12em">
-						<img class="image-in-container" src="https://cdn.shopify.com/s/files/1/1734/8431/products/Flying_1024x1024.jpg?v=1491498378"
-						style="width: 9em; height: 12em">
-						<img class="image-in-container" src="https://i.pinimg.com/originals/28/72/54/2872549acfb2e0ee8846edc3f973fd2a.jpg"
-						style="width: 9em; height: 12em">
+					 	<img class="image-in-container" src="https://i.pinimg.com/originals/f6/5a/53/f65a53be9515951bf9e28504efc5d6b0.jpg">
+						<img class="image-in-container" src="https://cdn.shopify.com/s/files/1/1734/8431/products/Flying_1024x1024.jpg?v=1491498378">
+						<img class="image-in-container" src="https://i.pinimg.com/originals/28/72/54/2872549acfb2e0ee8846edc3f973fd2a.jpg">
 				</div>
 				<div>
-						<img  class="image-in-container" src="https://images.discerningassets.com/image/upload/c_fit,h_1000,w_1000/c_fit,fl_relative,h_1.0,o_100,w_1.0/v1559837064/Giraffe_Gerald_qh7k1j.jpg"
-						style="width: 9em; height: 12em">
-						<img  class="image-in-container" src="https://i.pinimg.com/originals/07/4c/e9/074ce9b21ed92eaaa6a759dd3863b069.jpg"
-						style="width: 9em; height: 12em">
-						<img  class="image-in-container" src="https://cdn.shopify.com/s/files/1/1734/8431/products/White_Rooster_1024x1024.jpg?v=1581350689"
-						style="width: 9em; height: 12em;">
+						<img  class="image-in-container" src="https://images.discerningassets.com/image/upload/c_fit,h_1000,w_1000/c_fit,fl_relative,h_1.0,o_100,w_1.0/v1559837064/Giraffe_Gerald_qh7k1j.jpg">
+						<img  class="image-in-container" src="https://i.pinimg.com/originals/07/4c/e9/074ce9b21ed92eaaa6a759dd3863b069.jpg">
+						<img  class="image-in-container" src="https://cdn.shopify.com/s/files/1/1734/8431/products/White_Rooster_1024x1024.jpg?v=1581350689">
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="full-width-first" style="background-color: white; color: #080d52;">
+	<div class="full-width-first" class="white-field">
 		<div class="wrap-text">
 			<p style="font-size: 2em;">Join today as a...</p>
 		</div>
 		
 	</div>
-	<div class="full-width-first" style="background-color: white; color: #080d52;">
+	<div class="full-width-first" class="white-field">
 
 	  <div class="wrap">
 	  	<div>
 	  		<i class="fa fa-video-camera" aria-hidden="true"></i>
 	  		<span>Video Creator</span>
-	  		<p style="max-width: 8em; text-align: justify-all;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  	<div>
 	  		<i class="fa fa-paint-brush" aria-hidden="true"></i>
 	  		<span>Visual Artist</span>
-	  		<p style="max-width: 8em; text-align:justify-all; ;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  	<div>
 	  		<i class="fa fa-podcast" aria-hidden="true"></i>
 	  		<span>Podcaster</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  	<div>
 	  		<i class="fa fa-music" aria-hidden="true"></i>
 	  		<span>Musician</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  	<div>
 	  		<i class="fa fa-book" aria-hidden="true"></i>
 	  		<span>Writer/Journalist</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  </div>
 	  <div class="wrap" style="padding-bottom: 4em">
 	  	<div>
 	  		<i class="fa fa-gamepad" aria-hidden="true"></i>
 	  		<span>Gaming Creator</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  	<div>
 	  		<i class="fa fa-cutlery" aria-hidden="true"></i>
 	  		<span>Culinary artist</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  		  	<div>
 	  		<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 	  		<span>Local Buissines</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  	<div>
 	  		<i class="fa fa-handshake-o" aria-hidden="true"></i>
 	  		<span>Non-profit</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  	<div>
 	  		<i class="fa fa-plus-square" aria-hidden="true"></i>
 	  		<span>Something else</span>
-	  		<p style="max-width: 8em; text-align:justify-all;;">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
 	  	</div>
 	  </div>
 	</div>
 
-	<div class="full-width-first" style="background-color: #080d52; color: white">
+	<div class="full-width-first" class="dark-field">
 	  <div class="wrap">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero</div>
 	</div>
 </div>
+</body>
 <style>
+.slogan{
+	max-width: 16em; 
+	font-size: 1.1em;
+}
+.motto{
+	style="text-align: left; 
+	font-size: 3.1em;
+}
 .button-join {
 	border-radius: 26px;
 	background-color: #e0f4ff;
@@ -114,11 +137,17 @@
 }
 .image-in-container{
 	padding: 0.3em;
+	width: 9em;
+    height: 12em;
 }
 .image-containter {
 	display: flex;
-	flex-wrap: wrap;
+
 	 
+}
+.explain-content-type{
+	max-width: 8em;
+	text-align: justify-all;
 }
 .wrap {
 	justify-content: space-between;
@@ -126,54 +155,40 @@
 	display: flex;
 	flex-wrap: nowrap;	
 	flex-direction: row;
-	width: 80%;
+	width: 70%;
 	max-width: 50em;
 	margin: 0 auto;
 	text-align: left;
 	padding: 1em;
-
 	
-	
+}
+.white-field{
+	background-color: white; 
+	color: #080d52;
 }
 .wrap-text{
 	justify-content: center;
 	align-items: center;
 	display: flex;
-	flex-wrap: nowrap;	
+	flex-wrap: wrap;	
 	flex-direction: row;
-	width: 80%;
+	width: 30%;
 	max-width: 50em;
 	margin: 0 auto;
 	text-align: left;
 	
-
-	
-	
 }
-html, body {
-  overflow: hidden;
-}
-
-.full-width {
-	width: 100vw;
-	display: block;
-	position: relative;
-	left: 50%;
-	right: 50%;
-	margin: 0;
-	margin-left: -50vw;
-	background-color: white;
-	margin-right: -50vw;
+.dark-field{
+	background-color: #080d52;
+	color: white
 }
 .full-width-first {
 	top: 2.5em;
 	width: 100vw;
 	position: relative;
-	left: 50%;
-	color: white;
+	left: 56.2em;
 	right: 50%;
 	margin-left: -50vw;
-	background-color: #080d52;
 	margin-right: -50vw;
 }
 </style>
