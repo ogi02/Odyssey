c3e40204a54f236c409bc28303ed61cf64359a0a
Ognian Baruh <ognian@baruh.net>
2020-05-01 23:25:40 +0300

    Finish backend part of creating posts with image

--- odyssey/flask_post/post_actions.py
+++ odyssey/flask_post/post_actions.py
@@ -1,4 +1,5 @@
-# Built-in imports 
+# Built-in imports
+import os
 import json
 import datetime
 from bson import json_util, ObjectId
@@ -7,45 +8,70 @@ from bson import json_util, ObjectId
 from flask import Blueprint, request, jsonify
 
 # Imports from .py files
+from helpers import allowed_image
 from flask_classes.user import User
+from flask_classes.post import Post
 from flask_classes.active_user import ActiveUser
 from flask_logging.log_config import info_log, error_log
-from flask_classes.post import Post
+
+upload_folder = "./client/public/images"
 
 post_actions_bp = Blueprint('post_actions_bp', __name__)
 
 @post_actions_bp.route('/createPost', methods=['POST'])
 def create_post():
 	# Get user from session
-	user = User.get_from_db(ActiveUser.username)
+	username = ActiveUser.username
+	user = User.get_from_db(username)
 	user_id = user.get('_id')
 
-	# Get information about the future post
-	result = request.get_json().get('result')
-	
-	# Tuple with user's information
-	values = (
-		None,
-		user_id,
-		None,
-		datetime.datetime.now(),
-		result.get('image_path'),
-		result.get('text'),
-		result.get('restriction_type_id')
-	)
-	Post(*values).create()
 
-	info_log.info("%s added a new post" % ActiveUser.username)
-	
-	return jsonify(success=True, message='Successfully created a new post!')
+	# Get image from request
+	image = request.files["image"]
 
-@post_actions_bp.route('/createPostWithImage', methods=['POST'])
-def create_post_with_image():
-	# Get user from session
-	user = User.get_from_db(ActiveUser.username)
-	user_id = user.get('_id')
+	# Check image type
+	description = request.args.get("description")
+	required_id = request.args.get("required_id")
 
+	# Get time of post creation
+	now = datetime.datetime.now()
 	
+	# Validate image
+	if allowed_image(image.filename):
+
+		# Tuple with user's information
+		values = (
+			None,
+			user_id,
+			None,
+			now, # date
+			now.strftime("%m-%d-%Y-%H-%M-%S"), # image_path
+			description,
+			required_id
+		)
+		Post(*values).create()
+
+		# Define path where the image will go ("public/images/{user"s username}/)
+		path = os.path.join(upload_folder, username)
+
+		# Name for the picture
+		filename = now
+		
+		# Check if path exists and create one if it doesn"t
+		if not os.path.exists(path):
+			os.makedirs(path)
+
+		# Save image
+		image.save(os.path.join(path, now.strftime("%m-%d-%Y-%H-%M-%S")))
+		info_log.info("%s added a new post" % username)
+	
+		return jsonify(success=True, message='Successfully created a new post!')
+		
+	else:
+		error_log.error("Image extension is not allowed or doesn't exist!")
+
+		return jsonify(success=False, message="Allowed extensions: 'pdf', 'png', 'jpeg', 'jpg', 'gif'"), 403
+
 
 @post_actions_bp.route('/getLikeCount', methods=['POST'])
 def get_like_count():
