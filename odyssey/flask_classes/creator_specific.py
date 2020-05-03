5520cf2bc5bf0c64dd686ae9200036193eae06dc
Ognian Baruh <ognian@baruh.net>
2020-05-03 18:47:03 +0300

    [Fix] Update client and db in Tier class and CreatorSpecific class

--- odyssey/flask_classes/creator_specific.py
+++ odyssey/flask_classes/creator_specific.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
 from bson import ObjectId
 from pymongo import ReturnDocument
-client = MongoClient("mongodb+srv://KelpieG:admin11@clusterodyssey-olnzj.mongodb.net/test?retryWrites=true&w=majority")
-db = client.creator_specific
+
 class CreatorSpecific:
+
+	client = MongoClient("mongodb+srv://KelpieG:admin11@clusterodyssey-olnzj.mongodb.net/test?retryWrites=true&w=majority")
+	db = client.creator_specific
+
 	def __init__(self, _id, user_id, followers, patreons, content_type):
 		self._id = _id;
 		self.user_id = user_id
@@ -19,25 +22,25 @@ class CreatorSpecific:
 			'content_type': self.content_type
 
 		}
-		result = db.creator_specifics_collection.insert_one(creator_specific)
+		result = CreatorSpecific.db.creator_specifics_collection.insert_one(creator_specific)
 		return self
 		
 	def find_by_user_id(user_id):
 		user_id = ObjectId(user_id)
-		found = db.creator_specifics_collection.find_one({'user_id': user_id})
+		found = CreatorSpecific.db.creator_specifics_collection.find_one({'user_id': user_id})
 		if found:
 			return found
 
 	def get_follower_count(user_id):
 		user_id = ObjectId(user_id)
-		found = db.creator_specifics_collection.find_one({'user_id': user_id})
+		found = CreatorSpecific.db.creator_specifics_collection.find_one({'user_id': user_id})
 		if found:
 			info = Info(*found)
 			return info.followers.len()
 
 	def get_patreons_count(user_id):
 		user_id = ObjectId(user_id)
-		found = db.creator_specifics_collection.find_one({'user_id': user_id})
+		found = CreatorSpecific.db.creator_specifics_collection.find_one({'user_id': user_id})
 		if found:
 			info = Info(*found)
 			return info.patreons.len()
--- odyssey/flask_classes/tier.py
+++ odyssey/flask_classes/tier.py
@@ -2,9 +2,11 @@ from pymongo import MongoClient
 from bson import ObjectId
 from pymongo import ReturnDocument
 
-client = MongoClient("mongodb+srv://KelpieG:admin11@clusterodyssey-olnzj.mongodb.net/test?retryWrites=true&w=majority")
-db = client.tier
 class Tier:
+
+	client = MongoClient("mongodb+srv://KelpieG:admin11@clusterodyssey-olnzj.mongodb.net/test?retryWrites=true&w=majority")
+	db = client.tier
+
 	def __init__(self, _id, user_id, benefits, price, name):
 		self._id = _id
 		self.user_id = user_id
@@ -19,28 +21,27 @@ class Tier:
 			'benefits': self.benefits,
 			'price': self.price
 		}
-		result = db.tier_collection.insert_one(tier)
+		result = Tier.db.tier_collection.insert_one(tier)
 		return self
 	
 	def find_all_by_user_id(user_id):
 		user_id = ObjectId(user_id)
-		found = db.tier_collection.find({'user_id': user_id})
+		found = Tier.db.tier_collection.find({'user_id': user_id})
 		if found:
 			return found
 			
 	def find_by_name(user_id, name):
 		user_id = ObjectId(user_id)
-		found = db.tier_collection.find_one({'user_id': user_id, 'name': name})
+		found = Tier.db.tier_collection.find_one({'user_id': user_id, 'name': name})
 		if found:
 			return found
 
 	def find_by_id(tier_id):
 		tier_id = ObjectId(tier_id)
-		found = db.tier_collection.find_one({'_id': tier_id})
+		found = Tier.db.tier_collection.find_one({'_id': tier_id})
 		if found:
 			return found
 
 	def delete_tier(tier_id):
 		tier_id = ObjectId(tier_id)
-		db.tier_collection.delete_one({'_id': tier_id})
-		
+		Tier.db.tier_collection.delete_one({'_id': tier_id})
