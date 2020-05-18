efecf604fda4f6dbcb5025fbe406790149057f74
Ognian Baruh <ognian@baruh.net>
2020-05-18 10:47:37 +0300

    [New] Load more giveaways functionality with chunk of 10 giveaways

--- odyssey/client/src/Giveaways/MyGiveaways.svelte
+++ odyssey/client/src/Giveaways/MyGiveaways.svelte
@@ -1,10 +1,27 @@
 <script>
+	// Library imports
+	import { onMount } from "svelte";
+
 	// Javascript imports
 	import { fetchPost } from '../js/fetch.js';
 
 	// Inherited variables
 	export let giveaways;
 	export let user;
+
+	// Local variables
+	let loadedGiveaways = 0;
+	let allGiveawaysLoaded = false;
+
+	onMount(async () => {
+		if(user.role == "creator") {
+			// sort giveaways by date
+			giveaways.sort((a, b) => (a.date > b.date) ? -1 : 1);
+
+			// load more giveaways
+			await loadMoreGiveaways();
+		}
+	});
 
 	// Generate random giveaway winner
 	async function pickRandomGiveawayWinner(giveaway_id) {
@@ -48,83 +65,117 @@
 		giveaways = giveaways;
 	}
 
+	// Load more giveaways
+	async function loadMoreGiveaways() {
+		for(let i = loadedGiveaways; i < loadedGiveaways + 10; i++) {
+			// Check if there are any more giveaways to load
+			if(giveaways.length == (loadedGiveaways + i)) {
+				allGiveawaysLoaded = true;
+				loadedGiveaways += i;
+				return;
+			}
+		}
+
+		loadedGiveaways += 10;
+
+		// Remove button after last giveaway is loaded
+		if(giveaways.length == loadedGiveaways) {
+			allGiveawaysLoaded = true;
+			return;
+		}
+	}
+
 </script>
 
 <div class="giveaways-container">
 
-	{#each giveaways as giveaway}
-			
-		<div class='giveaway-box'>
+	{#each giveaways as giveaway, i}
 
-			<img class="giveaway-image" src={"/images/" + user.username + "/" + giveaway.image_path}>
-			
-			<div class="text-container">
-				<h3>{giveaway.text}</h3>
+		{#if i < loadedGiveaways}
+
+			<div class='giveaway-box'>
+
+				<img class="giveaway-image" src={"/images/" + user.username + "/" + giveaway.image_path}>
+				
+				<div class="text-container">
+					<h3>{giveaway.text}</h3>
+				</div>
+				
+				{#if giveaway.is_open == true}
+
+					<p>Number of contestants: </p>
+
+					{#await getParticipantCount(giveaway._id.$oid) then participants}
+
+						<p>{participants || 0}</p>
+
+						{#if participants}
+
+							<button on:click={async () => {
+								await closeGiveaway(giveaway._id.$oid);
+								let winner_id = await pickRandomGiveawayWinner(giveaway._id.$oid);
+								await chooseGiveawayWinner(giveaway._id.$oid, winner_id);
+							}}>Generate Winner</button>
+
+						{/if}
+
+					{/await}
+
+				{:else}
+					<p>{giveaway.winner}</p>
+				{/if}
+
 			</div>
-			
-			{#if giveaway.is_open == true}
 
-				<p>Number of contestants: </p>
-
-				{#await getParticipantCount(giveaway._id.$oid) then participants}
-
-					<p>{participants || 0}</p>
-
-					{#if participants}
-
-						<button on:click={async () => {
-							await closeGiveaway(giveaway._id.$oid);
-							let winner_id = await pickRandomGiveawayWinner(giveaway._id.$oid);
-							await chooseGiveawayWinner(giveaway._id.$oid, winner_id);
-						}}>Generate Winner</button>
-
-					{/if}
-
-				{/await}
-
-			{:else}
-				<p>{giveaway.winner}</p>
-			{/if}
-
-		</div>
+		{/if}
 
 	{/each}
+
+	{#if !allGiveawaysLoaded}
+		<button on:click={async () => await loadMoreGiveaways()}>Load More Giveaways</button>
+	{/if}
 
 </div>
 
 <style>
 
-	.giveaways-container{
-		display: flex;
+	.giveaways-container {
 		margin: 4em;
+		display: flex;
 		flex-shrink: 0;
 		flex-wrap: wrap;
 		justify-content: center;
 	}
 
 	.giveaway-box {
-		border: 2px solid #0f1930;
-		min-width: 40em;
-		max-width: 30em;
-		min-height: 23em;
+		width: 40em;
 		margin: 10px;
+		min-height: 23em;
+		border: 2px solid #0f1930;
 	}
 
 	.giveaway-image {
-		width: 40.2em;
-		margin-left: -2px;
-		margin: -3px;
 		height: 25em;
-		margin-top: -4px;
+		width: 40.2em;
+		margin: -4px -2px;
 	}
 
-	.text-container{
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
+	.text-container {
 		margin: 1em;
+		display: flex;
+		flex-wrap: wrap;
 		align-items: center;
-		justify-content: space-between;
+	}
+
+	button {
+		width: 12em;
+		color: white;
+		border: none;
+		cursor: pointer;
+		font-size: 18px;
+		margin-top: 1em;
+		border-radius: 50px;
+		background-color: #0f1930;
 	}
 
 </style>
