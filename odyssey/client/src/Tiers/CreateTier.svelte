12a98e138119d363c42d554e7538f369e6212ec9
Ognian Baruh <ognian@baruh.net>
2020-06-03 10:24:58 +0300

    [Update] Finished UI and code formatting on Create Tier form

--- odyssey/client/src/Tiers/CreateTier.svelte
+++ odyssey/client/src/Tiers/CreateTier.svelte
@@ -55,6 +55,10 @@
 		location.reload();
 	}
 
+	function cancelAddTier() {
+		addTierFlag = false;
+	}
+
 	function addBenefitField() {
 		// Add new empty benefit
 		benefits = benefits.concat('');
@@ -75,17 +79,17 @@
 	}
 
 	async function checkTierName() {
-		// Clear tier name error, enable 'Continue' button, show 'Loader' icon
-		clearError('name_error');
-		showLoader('loader_name');
-		enableButton('create');
-
 		// Check for empty tier name
 		if(checkEmptyTierName()) {
 			displayError('name_error', 'Tier name is required!');
 			disableButton('create');
 			return false;
 		}
+
+		// Clear tier name error, enable 'Continue' button, show 'Loader' icon
+		clearError('name_error');
+		showLoader('loader_name');
+		enableButton('create');
 
 		// Fetch post request for checking if such tier name exists in the database for the current user
 		const response = await fetchPost('http://localhost:3000/FpCerpd9Z7SIbjmN81Jy/tier_name', {
@@ -106,17 +110,17 @@
 	}
 
 	async function checkTierPrice() {
-		// Clear tier name error, enable 'Continue' button, show 'Loader' icon
-		clearError('price_error');
-		showLoader('loader_price');
-		enableButton('create');
-
 		// Check for empty tier price
 		if(checkEmptyTierPrice()) {
 			displayError('price_error', 'Price must be between 1 and 1000!');
 			disableButton('create');
 			return false;
 		}
+
+		// Clear tier name error, enable 'Continue' button, show 'Loader' icon
+		clearError('price_error');
+		showLoader('loader_price');
+		enableButton('create');
 
 		// Fetch post request for checking if such tier name exists in the database for the current user
 		const response = await fetchPost('http://localhost:3000/FpCerpd9Z7SIbjmN81Jy/tier_price', {
@@ -144,85 +148,98 @@
 
 		<h3>Add a New Tier!</h3>
 
-		<input type='text' id='name' placeholder='Name' bind:value={name}
-			on:input={async() => await checkTierName()}
-		>
+		<div class='field'>
 
-		<i id='loader_name' class='icons bx bx-loader'></i>
-		<Error id='name_error' message='' />
+			<input type='text' id='name' placeholder='Name' bind:value={name} class='input'
+				on:input={async() => await checkTierName()}
+			>
 
-		<input type='number' id='price' placeholder='Price' bind:value={price}
-			on:input={async() => await checkTierPrice()}
-		>
+			<i id='loader_name' class='icons bx bx-loader'></i>
+			<Error id='name_error' message='' />
 
-		<i id='loader_price' class='icons bx bx-loader'></i>
-		<Error id='price_error' message='' />
+		</div>
 
-		<button id='benefits_button' on:click|preventDefault={() => addBenefitField()}>Add benefit</button>
+		<div class='field'>
 
-		<Error id='benefits_error' message='' />
+			<input type='number' id='price' placeholder='Price' bind:value={price} class='input'
+				on:input={async() => await checkTierPrice()}
+			>
 
-		{#each benefits as benefit, i}
-			<div>
+			<i id='loader_price' class='icons bx bx-loader'></i>
+			<Error id='price_error' message='' />
 
-				<input type='text' class='benefits' bind:value={benefit} placeholder='Benefit'>
+		</div>
 
-				{#if benefits.length > 1}
-					<button id={i} class='remove' on:click|preventDefault={() => removeBenefit(i)}>Remove</button>
-				{/if}
+		<div class='benefits'>
 
-			</div>
-		{/each}
+			<button id='benefits_button' on:click|preventDefault={() => addBenefitField()}>Add benefit</button><br>
 
-		<p>Keep in mind that all of the benefits for the previous tiers will be applied for this tier.</p>
+			<Error id='benefits_error' message='' />
 
-		<button id='create' type='submit' on:click|preventDefault={async () => addTier()}>Continue</button>
+			{#each benefits as benefit, i}
+				<div class='benefit-field'>
+
+					<input type='text' class='benefit-input' bind:value={benefit} placeholder='Benefit'>
+
+					{#if benefits.length > 1}
+						<button id={i} class='remove' on:click|preventDefault={() => removeBenefit(i)}>Remove</button>
+					{/if}
+
+				</div>
+			{/each}
+
+			<p>Keep in mind that all of the benefits from the less expensive tiers will be applied for this tier.</p>
+
+		</div>
+
+		<div class='buttons'>
+
+			<button id='cancel' type='submit' on:click|preventDefault={() => cancelAddTier()}>Cancel</button>
+
+			<button id='create' type='submit' on:click|preventDefault={async () => addTier()}>Continue</button>
+
+		</div>
 
 	</form>
 
 </div>
 
+<link href='https://fonts.googleapis.com/css?family=Jost' rel='stylesheet'>
+
 <style>
 
-	/* Chrome, Safari, Edge, Opera */
-	input::-webkit-outer-spin-button,
-	input::-webkit-inner-spin-button {
-		-webkit-appearance: none;
-		margin: 0;
-	}
-
-	/* Firefox */
-	input[type=number] {
-		-moz-appearance: textfield;
-	}
-
 	.form {
-		margin-top: 80px;
-	}
-
-	input {
 		display: block;
-		width: 200px;
-	}
-
-	button {
-		display: block;
-	}
-
-	.remove, .benefits {
-		display: inline-block;
-	}
-
-	#benefits_error, #benefits_button {
-		display: inline;
+		font-size: 1.1em;
+		max-width: 400px;
+		margin: 100px auto;
+		text-align: center;
+		font-family: "Jost";
 	}
 
 	.input {
+		padding: 10px;
 		display: block;
+		min-width: 300px;
+		border-radius: 5px;
+		font-weight: 100px;
+		margin: 10px 0 2px;
+		outline-color: #bcdcfa;
+	}
+
+	.benefits {
+		width: 300px;
+		margin: 2em auto 0;
+	}
+
+	.benefit-input, .remove {
+		display: inline;
+	}
+
+	.benefit-input {
 		padding: 10px;
 		border-radius: 5px;
 		font-weight: 100px;
-		min-width: 300px;
 		margin: 10px 0 2px;
 		outline-color: #bcdcfa;
 	}
@@ -232,12 +249,65 @@
 	}
 
 	.icons {
-		position: absolute;
-		color: rgba(0, 0, 0, 0,7);
-		font-size: 16px;
-		display: none;
 		top: 25px;
 		right: 10px;
+		display: none;
+		font-size: 16px;
+		position: absolute;
+		color: rgba(0, 0, 0, 0,7);
+	}
+
+	.field {
+		width: 300px;
+		margin: 0 auto;
+		position: relative;
+	}
+
+	button {
+		border-radius: 5px;
+	}
+
+	button:not(.remove) {
+		width: 300px;
+	}
+
+	.buttons {
+		width: 300px;
+		display: flex;
+		margin: 0 auto;
+	}
+
+	#benefits_button {
+		background-color: #fff4f9;
+	}
+
+	#benefits_button:hover {
+		cursor: pointer;
+		background-color: #fff0f5;
+	}
+
+	#cancel {
+		color: #0f1931;
+		font-weight: bold;
+		margin-right: 5px;
+		background-color: #e6afcc;
+	}
+
+	#cancel:hover {
+		cursor: pointer;
+		background-color: #eca1c9;
+	}
+
+	#create {
+		color: #0f1931;
+		font-weight: bold;
+		margin-left: 5px;
+		background-color: #9fcdf5;
+	}
+
+	#create:hover {
+		cursor: pointer;
+		background-color: #8cc2f2;
 	}
 
 </style>
