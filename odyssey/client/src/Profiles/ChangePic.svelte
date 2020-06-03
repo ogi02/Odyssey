b5c75c42645bb1cea965a5bc9315e21bdf7eb764
Ognian Baruh <ognian@baruh.net>
2020-06-03 19:57:06 +0300

    [Update] Updated UI and code styling/formatting for change profile and cover picture

--- odyssey/client/src/Profiles/ChangePic.svelte
+++ odyssey/client/src/Profiles/ChangePic.svelte
@@ -45,19 +45,132 @@
 		src = 'images/' + username + '/' + type + '_picture?t=' + new Date().getTime();
 	}
 
+	function removeFile() {
+		files = null;
+	}
+
+	function cancelUploadPic() {
+		change = false;
+	}
+
 </script>
 
 <div class='card'>
 	
 	<form autocomplete='off'>
-		
-		<h3>Change your {type} picture!</h3>
-		
+
+		<label for='img' class='custom-file-upload'>
+			<i class='fa fa-cloud-upload'></i> Upload your new {type} picture!
+		</label>
+		{#if files}
+			<div class='filename'>
+				<p>{files[0].name}</p>
+				<i class='fa fa-trash-o icons' on:click={() => removeFile()}></i>
+			</div>
+		{/if}
 		<input type='file' id='img' bind:files>
 		
 		<Error id='picture_error' message='' />
 		
-		<button type='submit' on:click|preventDefault={() => uploadPic()}>Continue</button>
-	
+		<div class='buttons'>
+
+			<button type='submit' id='cancel' on:click|preventDefault={() => cancelUploadPic()}>Cancel</button>
+
+			<button type='submit' id='create' on:click|preventDefault={async () => uploadPic()}>Continue</button>
+
+		</div>
+
 	</form>
+
 </div>
+
+<link href='https://fonts.googleapis.com/css?family=Jost' rel='stylesheet'>
+
+<style>
+
+	.card {
+		max-width: 300px;
+		font-size: 1.1em;
+		margin: 100px auto;
+		text-align: center;
+		font-family: "Jost";
+	}
+
+	input[type=file] {
+		display: none;
+	}
+
+	.custom-file-upload {
+		max-width: 300px;
+		padding: 10px;
+		display: block;
+		margin: 0 auto;
+		border: 1px solid #333;
+	}
+
+	.custom-file-upload:hover {
+		cursor: pointer;
+		background-color: #f6f6f6;
+	}
+
+	.filename {
+		display: flex;
+		margin-top: 5px;
+		margin-bottom: 0;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.filename p {
+		margin: 0;
+		width: 250px;
+		overflow: hidden;
+		white-space: nowrap;
+		display: inline-block;
+		text-overflow: ellipsis;
+	}
+
+	.icons {
+		display: inline;
+	}
+
+	.icons:hover {
+		cursor: pointer;
+	}
+
+	button {
+		width: 300px;
+		border-radius: 5px;
+	}
+
+	.buttons {
+		width: 300px;
+		display: flex;
+		margin: 20px auto;
+	}
+
+	#cancel {
+		color: #0f1931;
+		font-weight: bold;
+		margin-right: 5px;
+		background-color: #e6afcc;
+	}
+
+	#cancel:hover {
+		cursor: pointer;
+		background-color: #eca1c9;
+	}
+
+	#create {
+		color: #0f1931;
+		font-weight: bold;
+		margin-left: 5px;
+		background-color: #9fcdf5;
+	}
+
+	#create:hover {
+		cursor: pointer;
+		background-color: #8cc2f2;
+	}
+
+</style>
