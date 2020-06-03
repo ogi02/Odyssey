92db619e6a54a7cb7bd098844b66bc167858f400
Ognian Baruh <ognian@baruh.net>
2020-06-03 23:17:16 +0300

    [Update] Finished UI and code styling/formatting for Create Survey form

--- odyssey/client/src/Surveys/CreateSurvey.svelte
+++ odyssey/client/src/Surveys/CreateSurvey.svelte
@@ -71,6 +71,14 @@
 		location.reload();
 	}
 
+	function removeFile() {
+		files = null;
+	}
+
+	function cancelCreateSurvey() {
+		createSurveyFlag = false;
+	}
+
 	function addOptionField() {
 		// Add new empty benefit
 		options = options.concat('');
@@ -85,14 +93,27 @@
 
 <div class='form'>
 
-	<form autocomplete="off">
+	<form autocomplete='off'>
 
 		<h3>Create a new survey!</h3>
 
-		<textarea id='description' placeholder='Description' bind:value={description}></textarea>
+		<textarea id='description' placeholder='Description' bind:value={description} rows='5' cols='40'></textarea>
 
-		<p>Choose a photo for the survey</p>
-		<input type='file' id='img' bind:files>
+		<div class='field'>
+
+			<label for='img' class='custom-file-upload'>
+				<i class='fa fa-cloud-upload'></i> Upload Survey image
+			</label>
+
+			{#if files}
+				<div class='filename'>
+					<p>{files[0].name}</p>
+					<i class='fa fa-trash-o icons' on:click={() => removeFile()}></i>
+				</div>
+			{/if}
+			<input type='file' id='img' bind:files>
+
+		</div>
 
 		<Error id='picture_error' message='' />
 
@@ -101,48 +122,162 @@
 		{#each tiers as tier, i}
 			<div>
 				{#if i == 0}
-					<input type="radio" id={tier._id.$oid} name="tier" value={tier._id.$oid} checked="checked">
+					<input type='radio' id={tier._id.$oid} name='tier' value={tier._id.$oid} checked='checked'>
 				{:else}
-					<input type="radio" id={tier._id.$oid} name="tier" value={tier._id.$oid}>
+					<input type='radio' id={tier._id.$oid} name='tier' value={tier._id.$oid}>
 				{/if}
 
 				<label for={tier._id.$oid}>{tier.name} ({tier.price}$)</label>
 			</div>
 		{/each}
 
-		<p>Options for survey</p>
+		<div class='field'>
 
-		<button id='options_button' on:click|preventDefault={() => addOptionField()}>Add option</button>
+			<p>Options for survey</p>
 
-		<Error id='options_error' message='' />
+			<button id='options_button' on:click|preventDefault={() => addOptionField()}>Add option</button>
 
-		{#each options as option, i}
-			<div>
+			<Error id='options_error' message='' />
 
-				<input type='text' class='options' bind:value={option} placeholder='Option'>
-			
-				{#if options.length > 1}
-					<button id={i} class='remove' on:click|preventDefault={() => removeOption(i)}>Remove</button>
-				{/if}
+			{#each options as option, i}
+				<div>
 
-			</div>
-		{/each}
+					<input type='text' class='options' bind:value={option} placeholder='Option'>
+				
+					{#if options.length > 1}
+						<button id={i} class='remove' on:click|preventDefault={() => removeOption(i)}>Remove</button>
+					{/if}
 
-		<button type='submit' on:click|preventDefault={async () => createSurvey()}>Create Survey</button>
+				</div>
+			{/each}
+
+		</div>
+
+		<div class='buttons'>
+
+			<button type='submit' id='cancel' on:click|preventDefault={() => cancelCreateSurvey()}>Cancel</button>
+
+			<button type='submit' id='create' on:click|preventDefault={async () => createSurvey()}>Create Survey</button>
+
+		</div>
 
 	</form>
 
 </div>
 
+<link href='https://fonts.googleapis.com/css?family=Jost' rel='stylesheet'>
+
 <style>
 
 	.form {
-		margin-top: 80px;
+		max-width: 400px;
+		font-size: 1.1em;
+		margin: 100px auto;
+		text-align: center;
+		font-family: "Jost";
+	}
+
+	.field {
+		width: 300px;
+		margin: 0 auto;
+		position: relative;
+	}
+
+	input[type=file] {
+		display: none;
+	}
+
+	.custom-file-upload {
+		padding: 10px;
+		display: block;
+		margin: 0 auto;
+		border: 1px solid #333;
+	}
+
+	.custom-file-upload:hover {
+		cursor: pointer;
+		background-color: #f6f6f6;
+	}
+
+	.filename {
+		display: flex;
+		margin-top: 5px;
+		margin-bottom: 0;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.filename p {
+		margin: 0;
+		width: 250px;
+		overflow: hidden;
+		white-space: nowrap;
+		display: inline-block;
+		text-overflow: ellipsis;
+	}
+
+	.icons {
+		display: inline;
+	}
+
+	.icons:hover {
+		cursor: pointer;
+	}
+
+	.tier-paragraph {
+		margin-top: 30px;
 	}
 
 	input[type=radio],
 	label {
 		display: inline-block;
+	}
+
+	button:not(.remove) {
+		width: 300px;
+	}
+
+	button {
+		border-radius: 5px;
+	}
+
+	.buttons {
+		width: 300px;
+		display: flex;
+		margin: 20px auto;
+	}
+
+	#options_button {
+		background-color: #fff4f9;
+	}
+
+	#options_button:hover {
+		cursor: pointer;
+		background-color: #fff0f5;
+	}
+
+	#cancel {
+		color: #0f1931;
+		font-weight: bold;
+		margin-right: 5px;
+		background-color: #e6afcc;
+	}
+
+	#cancel:hover {
+		cursor: pointer;
+		background-color: #eca1c9;
+	}
+
+	#create {
+		color: #0f1931;
+		font-weight: bold;
+		margin-left: 5px;
+		background-color: #9fcdf5;
+	}
+
+	#create:hover {
+		cursor: pointer;
+		background-color: #8cc2f2;
 	}
 
 </style>
