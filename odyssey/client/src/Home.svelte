2134f3f564aef6dbb2b28c8e225cd6dd97f2e4d8
Gabi <gabrielay2002@gmail.com>
2020-06-03 22:17:57 +0300

    [Update] Final UI for homepage

--- odyssey/client/src/Home.svelte
+++ odyssey/client/src/Home.svelte
@@ -1,3 +1,6 @@
+<script>
+	import router from "page";
+</script>
 
 <head>
 	<style>
@@ -25,7 +28,7 @@
 			<div>
 				<p class="motto">Be creative!</p>
 		  		<p class="slogan">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-		  		<button class="button-join">Join Odyssey today!</button>
+		  		<button class="button-join" on:click={() => router.redirect('/login')}>Join Odyssey today!</button>
 			</div>
 			<div class="image-container">
 				<div>
@@ -41,72 +44,83 @@
 			</div>
 		</div>
 	</div>
-	<div class="full-width-first white-field">
-		<div class="wrap-text">
-			<p style="font-size: 2em;">Join today as a...</p>
+	<div style="background-image: url('https://scontent.fsof8-1.fna.fbcdn.net/v/t1.15752-9/102563045_275731906911257_4286511703690051584_n.png?_nc_cat=102&_nc_sid=b96e70&_nc_ohc=1xRNFs4pxDwAX8dwCQN&_nc_ht=scontent.fsof8-1.fna&oh=e445d6d617cf691a91a2c6078fbcc3db&oe=5EFD4C81'); background-size: 974px 944px">
+		<div class="full-width-join white-field">
+			<div class="wrap-text">
+				<p style="font-size: 2em;">Join today as a...</p>
+			</div>
+			
 		</div>
-		
-	</div>
-	<div class="full-width-first white-field">
+		<div class="full-width-first white-field">
 
+		  <div class="wrap">
+		  	<div>
+		  		<i class="fa fa-video-camera" aria-hidden="true"></i>
+		  		<span>Video Creator</span>
+		  		<p class="explain-content-type">Develop new work with the support of your biggest fans, connect with your community in a troll-free environment, and build a predictable revenue stream that doesn’t rely on ads or algorithms.</p>
+		  	</div>
+		  	<div>
+		  		<i class="fa fa-paint-brush" aria-hidden="true"></i>
+		  		<span>Visual Artist</span>
+		  		<p class="explain-content-type">Earn more than just commissions alone by offering things like behind-the-scenes access, raw files, & walk-throughs of your creative process. Don't rely exclusively on unpredictable revenue streams.</p>
+		  	</div>
+		  	<div>
+		  		<i class="fa fa-podcast" aria-hidden="true"></i>
+		  		<span>Podcaster</span>
+		  		<p class="explain-content-type">Get to know your most loyal listeners and give them a place to connect, build community, and engage more deeply with you. Deliver bonus content, and establish a reliable revenue stream beyond ads.</p>
+		  	</div>
+		  	<div>
+		  		<i class="fa fa-music" aria-hidden="true"></i>
+		  		<span>Musician</span>
+		  		<p class="explain-content-type">Connect directly and build community with your most engaged fans. Test new work in a supportive environment. Establish a reliable, recurring revenue stream. Waiting to release an album? You call the shots.</p>
+		  	</div>
+		  	<div>
+		  		<i class="fa fa-book" aria-hidden="true"></i>
+		  		<span>Writer/Journalist</span>
+		  		<p class="explain-content-type">Don't turn to clickbait to hit pageview targets. Focus on quality over quantity for your biggest readers. Focus your time and energy writing for the people that value your content most.</p>
+		  	</div>
+		  </div>
+		  <div class="wrap" style="padding-bottom: 4em">
+		  	<div>
+		  		<i class="fa fa-gamepad" aria-hidden="true"></i>
+		  		<span>Gaming Creator</span>
+		  		<p class="explain-content-type">Reinvest funds to grow your community in a sustainable way. Focus your time and energy on the members that add value to your community. Cut out the rest.</p>
+		  	</div>
+		  	<div>
+		  		<i class="fa fa-cutlery" aria-hidden="true"></i>
+		  		<span>Culinary artist</span>
+		  		<p class="explain-content-type">Gain a platform where you can engage on a deeper level with the members that matter most. Share exclusive shots of your work and your WIP when creating something out of scratch</p>
+		  	</div>
+		  		  	<div>
+		  		<i class="fa fa-shopping-basket" aria-hidden="true"></i>
+		  		<span>Local Business</span>
+		  		<p class="explain-content-type">By using out platform you can expand your business's customer range. Communicate better with your clients and learn directly from them how to expand your product range.</p>
+		  	</div>
+		  	<div>
+		  		<i class="fa fa-handshake-o" aria-hidden="true"></i>
+		  		<span>Non-profit</span>
+		  		<p class="explain-content-type">All charitable nonprofits benefit from public support and donations. What better way to expand your organization than to get to know more people who support your cause? </p>
+		  	</div>
+		  	<div>
+		  		<i class="fa fa-plus-square" aria-hidden="true"></i>
+		  		<span>Something else</span>
+		  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work. And share your work with the people that respect it.</p>
+		  	</div>
+		  </div>
+		</div>
+	</div>
+	<div class="full-width-second dark-field">
 	  <div class="wrap">
+	  	<p>© Odyssey™</p>
+	  	<p>mail.odyssey.2020@gmail.com</p>
 	  	<div>
-	  		<i class="fa fa-video-camera" aria-hidden="true"></i>
-	  		<span>Video Creator</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
+		  	<i class="fa fa-instagram fa-2x ico" aria-hidden="true"></i>
+		  	<i class="fa fa-facebook-official fa-2x ico" aria-hidden="true"></i>
+		  	<i class="fa fa-twitter-square fa-2x ico" aria-hidden="true"></i>
 	  	</div>
-	  	<div>
-	  		<i class="fa fa-paint-brush" aria-hidden="true"></i>
-	  		<span>Visual Artist</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  	<div>
-	  		<i class="fa fa-podcast" aria-hidden="true"></i>
-	  		<span>Podcaster</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  	<div>
-	  		<i class="fa fa-music" aria-hidden="true"></i>
-	  		<span>Musician</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  	<div>
-	  		<i class="fa fa-book" aria-hidden="true"></i>
-	  		<span>Writer/Journalist</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  </div>
-	  <div class="wrap" style="padding-bottom: 4em">
-	  	<div>
-	  		<i class="fa fa-gamepad" aria-hidden="true"></i>
-	  		<span>Gaming Creator</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  	<div>
-	  		<i class="fa fa-cutlery" aria-hidden="true"></i>
-	  		<span>Culinary artist</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  		  	<div>
-	  		<i class="fa fa-shopping-basket" aria-hidden="true"></i>
-	  		<span>Local Business</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  	<div>
-	  		<i class="fa fa-handshake-o" aria-hidden="true"></i>
-	  		<span>Non-profit</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  	<div>
-	  		<i class="fa fa-plus-square" aria-hidden="true"></i>
-	  		<span>Something else</span>
-	  		<p class="explain-content-type">Take back creative control. Gain freedom and stability with the support of your fans. Do your best creative work.</p>
-	  	</div>
-	  </div>
-	</div>
 
-	<div class="full-width-first dark-field">
-	  <div class="wrap">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero</div>
+</div>
+
 	</div>
 </div>
 </body>
@@ -136,7 +150,8 @@
 	border-width: 1px;
 }
 .image-in-container{
-	padding: 0.3em;
+	padding: 0.4em;
+	border-radius: 10px;
 	width: 9em;
     height: 12em;
 }
@@ -146,7 +161,7 @@
 	 
 }
 .explain-content-type{
-	max-width: 8em;
+	max-width: 10em;
 	text-align: justify-all;
 }
 .wrap {
@@ -156,15 +171,17 @@
 	flex-wrap: nowrap;	
 	flex-direction: row;
 	width: 70%;
-	max-width: 50em;
+	max-width: 55em;
 	margin: 0 auto;
 	text-align: left;
 	padding: 1em;
 	
 }
 .white-field{
-	background-color: white; 
 	color: #080d52;
+}
+.ico{
+	padding: 8px;
 }
 .wrap-text{
 	justify-content: center;
@@ -183,6 +200,24 @@
 	color: white
 }
 .full-width-first {
+	top: 2.5em;
+	width: 100%;
+	margin: 0;
+	padding: 0;
+	padding-top: 1em;
+	position: relative;
+	padding-bottom: 2em;
+
+}
+.full-width-second {
+	width: 100%;
+	margin: 0;
+	padding: 0;
+	position: relative;
+	padding-top: 0.5em;
+
+}
+.full-width-join{
 	top: 2.5em;
 	width: 100%;
 	margin: 0;
